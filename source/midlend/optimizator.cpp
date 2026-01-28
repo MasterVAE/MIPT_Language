caf08d632b9111bdd72b2edf994b3d754d90e04d
MasterVAE <voilov07@gmail.com>
2026-01-28 20:03:53 +0300

    middlend optimization update

--- source/midlend/optimizator.cpp
+++ source/midlend/optimizator.cpp
@@ -6,6 +6,9 @@
 static void OptimizeNode(TreeNode* node);
 static bool IsMathOperation(TreeNode* node);
 
+static void ConstantFolding(TreeNode* node);
+static void DeadCodeElimination(TreeNode* node);
+
 Tree* OptimizeTree(Tree* tree)
 {
     assert(tree);
@@ -18,36 +21,42 @@ static void OptimizeNode(TreeNode* node)
     if(!node) return;
     OptimizeNode(node->left);
     OptimizeNode(node->right);
+}
 
-    // Dead Code Elimination
-    if(CheckOperation(node, OP_LINE), CheckOperation(node->left, OP_RETURN))
-    {
-        NodeDestroy(node->right);
-        node->right = NULL;
-        return;
-    }
+static bool IsMathOperation(TreeNode* node)
+{
+    return CheckType(node, NODE_OPERATION) && (node->value.operation == OP_ADD || 
+                                               node->value.operation == OP_SUB ||
+                                               node->value.operation == OP_MUL ||
+                                               node->value.operation == OP_DIV);
+}
 
-    // Constant Convolution
+static void ConstantFolding(TreeNode* node)
+{
+    assert(node);
+
     if(IsMathOperation(node) &&
        CheckType(node->left, NODE_CONSTANT) &&
        CheckType(node->right, NODE_CONSTANT))
     {
         int value = 0;
+        int lval = node->left->value.constant;
+        int rval = node->right->value.constant;
         if(CheckOperation(node, OP_ADD))
         {
-            value = node->left->value.constant + node->right->value.constant;
+            value =  lval + rval;
         }
         else if(CheckOperation(node, OP_SUB))
         {
-            value = node->left->value.constant - node->right->value.constant;
+            value = lval - rval;
         }
         else if(CheckOperation(node, OP_MUL))
         {
-            value = node->left->value.constant * node->right->value.constant;
+            value = lval * rval;
         }
         else if(CheckOperation(node, OP_DIV))
         {
-            value = node->left->value.constant / node->right->value.constant;
+            value = lval / rval;
         }
 
         TreeNode* parent = node->parent;
@@ -69,10 +78,35 @@ static void OptimizeNode(TreeNode* node)
     }
 }
 
-static bool IsMathOperation(TreeNode* node)
+static void DeadCodeElimination(TreeNode* node)
 {
-    return CheckType(node, NODE_OPERATION) && (node->value.operation == OP_ADD || 
-                                               node->value.operation == OP_SUB ||
-                                               node->value.operation == OP_MUL ||
-                                               node->value.operation == OP_DIV);
+    assert(node);
+
+
+    // CODE AFTER RETURN
+    if(CheckOperation(node, OP_LINE), CheckOperation(node->left, OP_RETURN))
+    {
+        NodeDestroy(node->right);
+        node->right = NULL;
+        return;
+    }
+
+
+    // IF(0)
+    if(CheckOperation(node, OP_IF) && CheckType(node->left, NODE_CONSTANT) 
+                                   && node->left->value.constant == 0)
+    {
+        bool right = node->parent->right == node;
+        if(right)
+        {
+            node->parent->right = NULL;
+        }
+        else
+        {
+            node->parent->left = NULL;
+        }
+
+        NodeDestroy(node);
+        return;
+    }
 }
